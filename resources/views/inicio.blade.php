f4773323a4c2611f9366795c89d01e61328fda5b
kawezinLiru <kawe.ribeiro@etec.sp.gov.br>
2022-04-29 16:47:44 -0300

    first commit

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1">
+        <title>Produtos</title>
+    </head>
+    <body>
+        <h1>Produtos</h1>
+        
+        <form action="/cadastrar-produto" method="POST">
+            @csrf
+            <label for="lblNome">Nome:</label>
+            <input type="text" name="nome">
+            <label for="lblValor">Valor:</label>
+            <input type="text" name="valor">
+            <label for="lblQuantidade">Quantidade:</label>
+            <input type="text" name="estoque">
+            <br><br>
+            <button>Cadastrar</button>
+        </form>
+    </body>
+</html>
